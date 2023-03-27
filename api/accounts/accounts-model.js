1372947c3530d4b04e920db4c9997016f2e07d0b
BuseMutluoglu <busemutluoglu@gmail.com>
2023-03-27 17:55:12 +0300

    model tamamlandı

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,29 @@
+const db = require("../../data/db-config");
 const getAll = () => {
   // KODLAR BURAYA
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
+const create = (account) => {
   // KODLAR BURAYA
-}
+  return db("accounts").insert(account);
+};
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // KODLAR BURAYA
-}
+  await db("accounts").where("id", id).update(account);
+  return await getById(id);
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +31,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
