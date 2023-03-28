4523c3b103ef1f02c89b090d6f2d31f75e6bbeaa
BuseMutluoglu <busemutluoglu@gmail.com>
2023-03-28 10:21:56 +0300

    devam ediliyor

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,27 @@
-exports.checkAccountPayload = (req, res, next) => {
+const accountModel = require("./accounts-model");
+const yup = require("yup");
+
+const accountSchema = yup.object().shape({
+  name: yup.string.min(3, "").max(100, "").required(""),
+  budget: yup.number("").min(0, "").max(1000000, "").required(""),
+});
+exports.checkAccountPayload = async (req, res, next) => {
+  try {
+    if (req.body && req.body.name) {
+      req.body.name = req.body.name.trim();
+    }
+    await accountSchema.validate(req.body);
+  } catch (error) {
+    next(error);
+  }
   // KODLAR BURAYA
   // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
-}
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // KODLAR BURAYA
-}
+};
 
 exports.checkAccountId = (req, res, next) => {
   // KODLAR BURAYA
-}
+};
